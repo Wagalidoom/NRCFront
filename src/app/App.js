8eaf35cfa49aee55686b9891670ea2a465da7dbd
dassman <lukas-8@live.fr>
2023-05-27 20:09:25 +0200

    cleaning repo/remove unusued files

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import HomeV1 from "../pages/homeV1";
-import HomeV2 from "../pages/homeV2";
-import HomeV3 from "../pages/homeV3";
-import Blogs from "../pages/blogs";
-import BlogDetails from "../pages/blogDetails";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../assets/styles/themes";
 import React from 'react';
@@ -15,10 +11,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<HomeV1 />} exact />
-        <Route path="/home-two" element={<HomeV2 />} />
-        <Route path="/home-three" element={<HomeV3 />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/post" element={<BlogDetails />} />
       </Routes>
     </ThemeProvider>
     </ThemeContext.Provider>
