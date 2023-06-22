4c0bfe4e64ab68c5ea24fc041a66f37d259c81e9
Luka la d <lukas-8@live.fr>
2023-06-22 10:58:47 +0200

    add subgraph queries for color

--- src/context/ethereumProvider.js
+++ src/context/ethereumProvider.js
@@ -1,8 +1,15 @@
-import { createContext, useContext, useState } from 'react';
-import { ethers } from 'ethers';
-import { Multicall, ContractCallResults, ContractCallContext } from 'ethereum-multicall';
-import { NUMBERRUNNERCLUB_ABI } from '../ressources/abi';
+import { createContext, useContext, useState } from "react";
+import { ethers } from "ethers";
+import {
+  Multicall,
+  ContractCallResults,
+  ContractCallContext,
+} from "ethereum-multicall";
+import Axios from 'axios';
+import { NUMBERRUNNERCLUB_ABI } from "../ressources/abi";
 
+const NRCsubgraph =
+  "https://api.studio.thegraph.com/query/48701/nrctestnet/v0.0.3";
 const EthereumContext = createContext(null);
 
 export function EthereumProvider({ children }) {
@@ -14,33 +21,39 @@ export function EthereumProvider({ children }) {
     try {
       await ethereumState.contract.chooseColor(_color);
       setIsColorPickerOpen(false);
-      const mint = await ethereumState.contract.mint(5, "0x0", { value: ethers.utils.parseEther("0.2") }); // mint a Pawn
+      const mint = await ethereumState.contract.mint(5, "0x0", {
+        value: ethers.utils.parseEther("0.2"),
+      }); // mint a Pawn
       console.log(mint);
-    }
-    catch (error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   };
 
   const chooseBlackColor = async () => {
     await chooseColor(1);
-  }
+  };
 
   const chooseWhiteColor = async () => {
     await chooseColor(2);
-  }
+  };
 
   const mint = async (mintCount) => {
     setIsMintOpen(false);
 
     if (!ethereumState.contract) {
-      throw new Error('Contract is not defined');
+      throw new Error("Contract is not defined");
     }
 
-    const multicall = new Multicall({ ethersProvider: ethereumState.provider, tryAggregate: false });
+    const multicall = new Multicall({
+      ethersProvider: ethereumState.provider,
+      tryAggregate: false,
+    });
 
     for (let i = 0; i < mintCount; i++) {
-      multicall.call('mint', 5, "0x0", { value: ethers.utils.parseEther("0.2") });
+      multicall.call("mint", 5, "0x0", {
+        value: ethers.utils.parseEther("0.2"),
+      });
     }
 
     try {
@@ -50,7 +63,6 @@ export function EthereumProvider({ children }) {
       console.error(error);
     }
   };
-
 
   const checkIfStacked = async (_id) => {
     if (!ethereumState.contract) return false;
@@ -66,23 +78,24 @@ export function EthereumProvider({ children }) {
   const stack = async (_id) => {
     if (!ethereumState.contract) return;
     await ethereumState.contract.approve(ethereumState.contract.address, _id);
-    let tx = await ethereumState.contract.stack(ethers.utils.formatBytes32String("121.eth"), _id);
+    let tx = await ethereumState.contract.stack(
+      ethers.utils.formatBytes32String("121.eth"),
+      _id
+    );
     console.log(tx);
-
-  }
+  };
 
   const unstack = async (_id) => {
     if (!ethereumState.contract) return;
     let tx = await ethereumState.contract.unstack(_id);
     console.log(tx);
-
-  }
+  };
 
   const burn = async (_id) => {
     if (!ethereumState.contract) return;
     console.log(_id);
     await ethereumState.contract.burn(_id);
-  }
+  };
 
   const [ethereumState, setEthereumState] = useState({
     provider: null,
@@ -96,11 +109,15 @@ export function EthereumProvider({ children }) {
     }
 
     try {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({ method: "eth_requestAccounts" });
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contractAddress = "0x56DFe0ca7c483aB7211699826d0216A47B3D351e";
-      const contract = new ethers.Contract(contractAddress, NUMBERRUNNERCLUB_ABI, signer);
+      const contract = new ethers.Contract(
+        contractAddress,
+        NUMBERRUNNERCLUB_ABI,
+        signer
+      );
       setEthereumState({ provider, contract });
       console.log(window.ethereum);
       console.log("Wallet Connected !");
@@ -115,17 +132,37 @@ export function EthereumProvider({ children }) {
     if (!ethereumState.provider || !ethereumState.contract) return;
     console.log("Mint Pawn");
     const address = await ethereumState.provider.getSigner().getAddress();
-    const hasColorChosen = await ethereumState.contract.getUserColor(address);
-    if (hasColorChosen === 0) {
+    let query = `
+    {
+      colorChooseds(where: { user: "${address}" }) {
+        id
+        color
+        user
+        blockNumber
+        blockTimestamp
+        transactionHash
+      }
+    }
+        `;
+        let hasColorChosen;
+    try {
+      await Axios.post(NRCsubgraph, { query: query }).then(
+        (result) => {
+          hasColorChosen = Object.values(result.data.data);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    if (hasColorChosen[0].length === 0) {
       setIsColorPickerOpen(true);
       console.log("display choose color component");
-    }
-    else {
-      setIsMintOpen(true)
+    } else {
+      setIsMintOpen(true);
       // const mint = await ethereumState.contract.mint(5, "0x0", { value: ethers.utils.parseEther("0.2") }); // mint a Pawn
-      console.log(mint);
+      console.log(hasColorChosen[0]);
     }
-  }
+  };
 
   const value = {
     ethereumState,
@@ -139,7 +176,7 @@ export function EthereumProvider({ children }) {
     burn,
     stack,
     unstack,
-    checkIfStacked
+    checkIfStacked,
   };
 
   return (
@@ -153,7 +190,7 @@ export function EthereumProvider({ children }) {
 export function useEthereum() {
   const context = useContext(EthereumContext);
   if (context === undefined) {
-    throw new Error('useEthereum must be used within a EthereumProvider')
+    throw new Error("useEthereum must be used within a EthereumProvider");
   }
   return context;
 }
