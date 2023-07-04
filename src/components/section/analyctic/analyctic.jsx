438854b6c4ff330e8ff6fcac4094fbe01086e26d
Jinter <jordan.cosio@outlook.fr>
2023-07-04 14:31:07 +0200

    - Added nfytype getter - Added subgraph queries on topholders

--- src/components/section/analyctic/analyctic.jsx
+++ src/components/section/analyctic/analyctic.jsx
@@ -1,8 +1,62 @@
 import { AnalycticContainer } from "./analyctic.style"
-import { useState } from "react"
+import { useState, useEffect } from 'react';
+import { NRCsubgraph, useEthereum } from "../../../context/ethereumProvider";
+import Axios from 'axios';
 import cavalier from "../../../assets/images/icon/IconCavalier-Blanc.png";
 export const Analyctic = ({ theme }) => {
     const [arrayTest, setArrayTest] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    // Définir l'état pour les NFTs
+    const [shares, setShares] = useState([]);
+
+    useEffect(() => {
+        const first = 6;
+        // Définition des requêtes
+        const GET_TOP_HOLDERS = `
+        {
+            nftSharesUpdateds (where: { shares_gt: 0 }) {
+              id
+              tokenId
+              shares
+            }
+        }
+        `;
+    
+        const GET_LAST_GLOBAL_SHARES = `
+        {
+            globalSharesUpdateds (first: 1, orderBy: blockNumber, orderDirection: desc) {
+              id
+              shares
+              blockNumber
+            }
+        }
+        `;
+    
+        // Fonction pour obtenir les NFTs
+        const fetchShares = async () => {
+          try {
+            // Faire les requêtes et obtenir les réponses
+            const responseHolders= await Axios.post(NRCsubgraph, { query: GET_TOP_HOLDERS });
+            const responseGlobalShares = await Axios.post(NRCsubgraph, { query: GET_LAST_GLOBAL_SHARES });
+    
+            // Obtenir les données
+            const topHolders = responseHolders.data.data.nftSharesUpdateds;
+            const lastGlobalShares = responseGlobalShares.data.data.globalSharesUpdateds.shares;
+
+            // Fusionner et trier les NFTs
+            // const rewards = topHolders.map((holder) => );
+            const sorted = merged.sort((a, b) => b.blockNumber - a.blockNumber);
+    
+            // Mettre à jour l'état avec les NFTs
+            setShares(sorted);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        // Appeler la fonction pour obtenir les NFTs
+        fetchShares();
+      }, []);
+
+
     const showMore = () => {
         let newData = []
         for (let index = 1; index <= 10; index++) {
@@ -10,6 +64,7 @@ export const Analyctic = ({ theme }) => {
         }
         setArrayTest(arrayTest.concat(newData))
     }
+
     return (
         <AnalycticContainer>
             <div>
--- src/utils/web3mint.js
+++ src/utils/web3mint.js
@@ -30,3 +30,21 @@ export const totalMintCount = async () => {
         return totalMint;
     }
 }
+
+export function getNftType(nftId) {
+    if (nftId >= 0 && nftId < 2) {
+        return 0;
+    } else if (nftId >= 2 && nftId < 12) {
+        return 1;
+    } else if (nftId >= 12 && nftId < 62) {
+        return 2;
+    } else if (nftId >= 62 && nftId < 162) {
+        return 3;
+    } else if (nftId >= 162 && nftId < 362) {
+        return 4;
+    } else if (nftId >= 362 && nftId < 10000) {
+        return 5;
+    } else {
+        return "Error on NFT type";
+    }
+}
