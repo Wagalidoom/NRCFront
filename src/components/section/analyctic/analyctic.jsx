af6502f53733e9d1119d58b7ce78c14e77bcdefd
Jinter <jordan.cosio@outlook.fr>
2023-07-04 17:10:29 +0200

    Updated analystics.js

--- src/components/section/analyctic/analyctic.jsx
+++ src/components/section/analyctic/analyctic.jsx
@@ -1,19 +1,21 @@
 import { AnalycticContainer } from "./analyctic.style"
 import { useState, useEffect } from 'react';
-import { NRCsubgraph, useEthereum } from "../../../context/ethereumProvider";
+import { NRCsubgraph } from "../../../context/ethereumProvider";
 import Axios from 'axios';
 import cavalier from "../../../assets/images/icon/IconCavalier-Blanc.png";
+import BigNumber from 'bignumber.js';
+import { getNftType } from "../../../helper";
+
 export const Analyctic = ({ theme }) => {
     const [arrayTest, setArrayTest] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
     // Définir l'état pour les NFTs
     const [shares, setShares] = useState([]);
-
+    
     useEffect(() => {
-        const first = 6;
         // Définition des requêtes
         const GET_TOP_HOLDERS = `
         {
-            nftSharesUpdateds (where: { shares_gt: 0 }) {
+            nftSharesUpdateds  {
               id
               tokenId
               shares
@@ -33,24 +35,27 @@ export const Analyctic = ({ theme }) => {
     
         // Fonction pour obtenir les NFTs
         const fetchShares = async () => {
-          try {
-            // Faire les requêtes et obtenir les réponses
-            const responseHolders= await Axios.post(NRCsubgraph, { query: GET_TOP_HOLDERS });
-            const responseGlobalShares = await Axios.post(NRCsubgraph, { query: GET_LAST_GLOBAL_SHARES });
-    
-            // Obtenir les données
-            const topHolders = responseHolders.data.data.nftSharesUpdateds;
-            const lastGlobalShares = responseGlobalShares.data.data.globalSharesUpdateds.shares;
+            try {
+                // Faire les requêtes et obtenir les réponses
+                const responseHolders = await Axios.post(NRCsubgraph, { query: GET_TOP_HOLDERS });
+                const responseGlobalShares = await Axios.post(NRCsubgraph, { query: GET_LAST_GLOBAL_SHARES });
+            
+                // Obtenir les données
+                const topHolders = responseHolders.data.data.nftSharesUpdateds;
+                const lastGlobalShares = responseGlobalShares.data.data.globalSharesUpdateds[0].shares;
 
-            // Fusionner et trier les NFTs
-            // const rewards = topHolders.map((holder) => );
-            const sorted = merged.sort((a, b) => b.blockNumber - a.blockNumber);
-    
-            // Mettre à jour l'état avec les NFTs
-            setShares(sorted);
-          } catch (error) {
-            console.error(error);
-          }
+                // Calculer récompenses
+                const rewards = topHolders.map((holder) => {
+                    const nftType = getNftType(holder.tokenId);
+                    console.log(holder.shares, lastGlobalShares[nftType]);
+                    holder.shares = lastGlobalShares[nftType] - holder.shares;
+                    return holder.shares;
+                });
+            
+                console.log(rewards);
+            } catch (error) {
+                console.error(error);
+            }
         };
         // Appeler la fonction pour obtenir les NFTs
         fetchShares();
