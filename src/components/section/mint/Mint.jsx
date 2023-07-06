a9d6e3eba18d13ba04f14798cb2f405181d732b5
Luka la d <lukas-8@live.fr>
2023-07-07 00:08:02 +0200

    update mint front

--- src/components/section/mint/Mint.jsx
+++ src/components/section/mint/Mint.jsx
@@ -1,7 +1,17 @@
 import { Button, Slider, TextField } from "@mui/material";
+import { styled } from '@mui/system';
 import { useEthereum } from "../../../context/ethereumProvider";
 import { MintStyleWrapper } from "./Mint.style";
+import CloseIcon from '@mui/icons-material/Close';
 import { useState } from "react";
+
+const CustomTextField = styled(TextField)({
+  '& .MuiInputBase-root': {
+    color: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: 'rgb(0 0 0)',
+    width: '50px'
+  },
+});
 
 export const Mint = () => {
     const { mint } = useEthereum();
@@ -15,18 +25,22 @@ export const Mint = () => {
         <MintStyleWrapper>
             <div className="callContractContainer">
                 <div className="mintContent">
-                    <div style={{ display: "flex", padding: "10px", justifyContent: "space-between", alignItems: "end" }}>
+                    <div style={{ display: "flex", padding: "10px", justifyContent: "space-between", alignItems: "center" }}>
                         Mint
-                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "end" }}>
+                        <div style={{ display: "flex", width: "40%", justifyContent: "flex-end", alignItems: "center" }}>
                             Items
-                            <TextField
+                            <CustomTextField
                                 inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                                 hiddenLabel
                                 id="filled-hidden-label-small"
                                 defaultValue={1}
                                 variant="filled"
                                 size="small"
+                                style={{margin: "0 15px"}}
                             />
+                            <button>
+                                <CloseIcon />                            
+                            </button>
                         </div>
                     </div>
                     <Slider
