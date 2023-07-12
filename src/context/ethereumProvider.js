4d7985534d459adf894de8cd84de88c4a2a0aee6
Luka la d <lukas-8@live.fr>
2023-07-12 15:41:30 +0200

    update subgraph

--- src/context/ethereumProvider.js
+++ src/context/ethereumProvider.js
@@ -13,7 +13,7 @@ export const ETHEREUM_RPC_URL =
   "https://eth-goerli.g.alchemy.com/v2/MGGlH-80oFX2RUjT-9F8pd6h6d3AG0hj";
 
 export const NRCsubgraph =
-  "https://api.studio.thegraph.com/query/48701/nrctestnet/69";
+  "https://api.studio.thegraph.com/query/48701/nrctestnet/0.2.28";
 
 export const ENSsubgraph =
   "https://api.thegraph.com/subgraphs/name/ensdomains/ensgoerli";
