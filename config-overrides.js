3098ec3f55da91b3ad10eabe5c6fbfa3516e9e00
Luka la d <lukas-8@live.fr>
2023-10-09 00:14:07 +0200

    fix polyfills

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,9 @@
 module.exports = function override(config, env) {
-    config.plugins.push(new NodePolyfillPlugin({
-      excludeAliases: ["console"]
-    }))
-    return config
-  }
+  config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "zlib": require.resolve("browserify-zlib")
+  };
+  return config;
+}
