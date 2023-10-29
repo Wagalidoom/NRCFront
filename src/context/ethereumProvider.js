ca7b51d0e8c7120de93d37e3dddd9fdf6a78469d
Luka la d <lukas-8@live.fr>
2023-10-29 15:15:21 +0100

    feat : update smart contract address and subgraph

--- src/context/ethereumProvider.js
+++ src/context/ethereumProvider.js
@@ -13,12 +13,12 @@ export const ETHEREUM_RPC_URL =
   "https://eth-goerli.g.alchemy.com/v2/MGGlH-80oFX2RUjT-9F8pd6h6d3AG0hj";
 
 export const NRCsubgraph =
-  "https://api.studio.thegraph.com/query/48701/nrctestnet/0.4.4";
+  "https://api.studio.thegraph.com/query/48701/nrctestnet/0.4.5";
 
 export const ENSsubgraph =
   "https://api.thegraph.com/subgraphs/name/ensdomains/ensgoerli";
 
-export const contractAddress = "0xb71823553D0b0DfAA0C266eD2a60798c474DdD5E";
+export const contractAddress = "0x9179A6dA94eDE723203Ab4c30d5Bd0afA2c59647";
 
 const EthereumContext = createContext(null);
 
